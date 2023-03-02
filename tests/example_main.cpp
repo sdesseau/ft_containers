f224651d164b79628f53a509e6136df3aa1c22c9
Stan <stan@jos-mac-mini.home>
2023-03-02 16:13:59 +0100

    testing

--- tests/example_main.cpp
+++ tests/example_main.cpp
@@ -1,6 +1,10 @@
 
 #include "test_include.hpp"
 
+
+// typedef TESTED_NAMESPACE::map<T1, T2>::iterator ft_iterator;
+// typedef TESTED_NAMESPACE::map<T1, T2>::const_iterator ft_const_iterator;
+
 // static int iter = 0;
 
 // template <typename MAP>
@@ -38,29 +42,29 @@ int main()
 	 /* Tests Vector */
 
 	// std::cout << std::endl << "                   New Test" << std::endl;
-    // test_assign();
+    test_assign();
 	// std::cout << std::endl << "                   New Test" << std::endl;
-    // test_bidirect_it();
+    test_bidirect_it();
 	// std::cout << std::endl << "                   New Test" << std::endl;
-    // test_size();
+    test_size();
 	// std::cout << std::endl << "                   New Test" << std::endl;
-    // test_erase();
+    test_erase();
 
 	// /* Test Iterator Vector */
 
 	// std::cout << std::endl << "                   New Test" << std::endl;
-    // test_ite();
+    test_ite();
 	// std::cout << std::endl << "                   New Test" << std::endl;
-	// test_ite_eq_ope();
+	test_ite_eq_ope();
 	// std::cout << std::endl << "                   New Test" << std::endl;
-	// test_rev_it();
+	test_rev_it();
 
 	// /* Test Map */
 
 	// std::cout << std::endl << "                   New Test" << std::endl;
-	// test_map_copyconstruct();
+	test_map_copyconstruct();
 	// std::cout << std::endl << "                   New Test" << std::endl;
-	// test_construct_hard();
+	test_construct_hard();
 
 	// TESTED_NAMESPACE::map<char,int> first;
 	// TESTED_NAMESPACE::map<char,int> second;
@@ -75,18 +79,18 @@ int main()
 	// std::cout << "Size of first: " << first.size() << '\n';
 	// std::cout << "Size of second: " << second.size() << '\n';
 
-	// TESTED_NAMESPACE::map<char,std::string> mymap;
+	// TESTED_NAMESPACE::map<char,std::string> mymap2;
 
-	// mymap['a']="an element";
-	// mymap['b']="another element";
-	// mymap['c']=mymap['b'];
+	// mymap2['a']="an element";
+	// mymap2['b']="another element";
+	// mymap2['c']=mymap2['b'];
 
-	// std::cout << "mymap['a'] is " << mymap['a'] << '\n';
-	// std::cout << "mymap['b'] is " << mymap['b'] << '\n';
-	// std::cout << "mymap['c'] is " << mymap['c'] << '\n';
-	// std::cout << "mymap['d'] is " << mymap['d'] << '\n';
+	// std::cout << "mymap2['a'] is " << mymap2['a'] << '\n';
+	// std::cout << "mymap2['b'] is " << mymap2['b'] << '\n';
+	// std::cout << "mymap2['c'] is " << mymap2['c'] << '\n';
+	// std::cout << "mymap2['d'] is " << mymap2['d'] << '\n';
 
-	// std::cout << "mymap now contains " << mymap.size() << " elements.\n";
+	// std::cout << "mymap2 now contains " << mymap2.size() << " elements.\n";
 
 
 	// TESTED_NAMESPACE::vector<int> v;
@@ -140,11 +144,11 @@ int main()
 	// 	v.insert(TESTED_NAMESPACE::pair<int, int>(i * 4, i));
 	
 	// // for (vector<int>::const_iterator it = v.begin(); it != v.end(); ++it)
-	// TESTED_NAMESPACE::map<int, int>::const_reverse_iterator it = v.rbegin();
-	// TESTED_NAMESPACE::map<int, int>::const_reverse_iterator ite = v.rend();
-	// while (it != ite){
-	// 	std::cout << (*(it)).first << std::endl;
-	// 	it++;
+	// TESTED_NAMESPACE::map<int, int>::const_reverse_iterator it2 = v.rbegin();
+	// TESTED_NAMESPACE::map<int, int>::const_reverse_iterator ite2 = v.rend();
+	// while (it2 != ite2){
+	// 	std::cout << (*(it2)).first << std::endl;
+	// 	it2++;
 	// }
     // std::cout << std::endl;
 
@@ -183,42 +187,42 @@ int main()
 
 
 
-// std::list<T3> lst;
-// 	unsigned int lst_size = 10;
-// 	for (unsigned int i = 0; i < lst_size; ++i)
-// 		lst.push_back(T3(i + 1, (i + 1) * 3));
-// 	TESTED_NAMESPACE::map<T1, T2> mp(lst.begin(), lst.end());
-// 	printSizeMap(mp);
+// std::list<T3> lst2;
+// 	unsigned int lst2_size2 = 10;
+// 	for (unsigned int i = 0; i < lst2_size2; ++i)
+// 		lst2.push_back(T3(i + 1, (i + 1) * 3));
+// 	TESTED_NAMESPACE::map<T1, T2> mp3(lst2.begin(), lst2.end());
+// 	printSizeMap(mp3);
 
-// 	ft_const_bound(mp, -10);
-// 	ft_const_bound(mp, 1);
-// 	ft_const_bound(mp, 5);
-// 	ft_const_bound(mp, 10);
-// 	ft_const_bound(mp, 50);
+// 	ft_const_bound(mp3, -10);
+// 	ft_const_bound(mp3, 1);
+// 	ft_const_bound(mp3, 5);
+// 	ft_const_bound(mp3, 10);
+// 	ft_const_bound(mp3, 50);
 
-// 	printSizeMap(mp);
+// 	printSizeMap(mp3);
 
-// 	mp.lower_bound(3)->second = 404;
-// 	mp.upper_bound(7)->second = 842;
-// 	ft_bound(mp, 5);
-// 	ft_bound(mp, 7);
+// 	mp3.lower_bound(3)->second = 404;
+// 	mp3.upper_bound(7)->second = 842;
+// 	ft_bound(mp3, 5);
+// 	ft_bound(mp3, 7);
 
-// 	printSizeMap(mp);
+// 	printSizeMap(mp3);
 
- TESTED_NAMESPACE::map<char,int> mymap;
+//  TESTED_NAMESPACE::map<char,int> mymap;
 
-  mymap['a']=10;
-  mymap['b']=20;
-  mymap['c']=30;
+//   mymap['a']=10;
+//   mymap['b']=20;
+//   mymap['c']=30;
 
-  TESTED_NAMESPACE::pair<TESTED_NAMESPACE::map<char,int>::iterator,TESTED_NAMESPACE::map<char,int>::iterator> ret;
-  ret = mymap.equal_range('b');
+//   TESTED_NAMESPACE::pair<TESTED_NAMESPACE::map<char,int>::iterator,TESTED_NAMESPACE::map<char,int>::iterator> ret;
+//   ret = mymap.equal_range('b');
 
-  std::cout << "lower bound points to: ";
-  std::cout << ret.first->first << " => " << ret.first->second << '\n';
+//   std::cout << "lower bound points to: ";
+//   std::cout << ret.first->first << " => " << ret.first->second << '\n';
 
-  std::cout << "upper bound points to: ";
-  std::cout << ret.second->first << " => " << ret.second->second << '\n';
+//   std::cout << "upper bound points to: ";
+//   std::cout << ret.second->first << " => " << ret.second->second << '\n';
   
 
 
@@ -246,5 +250,47 @@ int main()
 // 	}
 
 
+	// TESTED_NAMESPACE::map<std::string, T2> mapp;
 
+	// mapp["Bonbon"] = 10;
+	// mapp["Canard"] = 20;
+	// mapp["Durite"] = 30;
+	// mapp["Elephant"] = 40;
+	// TESTED_NAMESPACE::map<std::string, T2> mapp2(mapp);
+	// TESTED_NAMESPACE::map<std::string, T2>::iterator t2332 = mapp2.begin();
+
+	// TESTED_NAMESPACE::map<std::string, T2>::iterator t = mapp.begin();
+	// while (t != mapp.end())
+	// 	std::cout << "ite* = " << (*(t++)).second << std::endl;
+	// TESTED_NAMESPACE::map<std::string, T2>::iterator itt = mapp.begin();
+	// 	std::cout << "itttte* = " << (*(itt)).second << std::endl;
+	
+	// (*(itt)).second = 42;
+	// mapp.erase("Canard");
+	// t = mapp.begin();
+	// while (t != mapp.end())
+	// 	std::cout << "ite* = " << (*(t++)).second << std::endl;
+
+	// mapp.swap(mapp2);
+	// t = mapp.begin();
+	// while (t != mapp.end())
+	// 	std::cout << "ite* = " << (*(t++)).second << std::endl;
+	// std::cout << "itttte* = " << (*(itt)).second << std::endl;
+	// std::cout << "t2332* = " << (*(t2332)).second << std::endl;
+
+	TESTED_NAMESPACE::map<T1, T2> mapp;
+	for (int i = 0; i < 200000; i++)
+		mapp[i] = i * 4;
+	mapp.insert(mapp.begin(), mapp.end());
+	TESTED_NAMESPACE::map<T1, T2>::iterator t = mapp.begin();
+	// while (t != mapp.end())
+	// 	std::cout << "ite* = " << (*(t++)).second << std::endl;
+	for (int i = 0; i < 200000; i++)
+		mapp.erase(i);
+	for (int i = 0; i < 200000; i++)
+		mapp[i] = i * 4;
+	mapp.erase(mapp.begin(), mapp.end());
+	t = mapp.begin();
+	while (t != mapp.end())
+		std::cout << "ite* = " << (*(t++)).second << std::endl;
 }
